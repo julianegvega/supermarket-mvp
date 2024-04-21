4d15af5850a194517faf2a741b10801eea266274
Julian Vega <juliangomvega@gmail.com>
2024-04-21 00:55:25 -0500

    Product Repository

---
+++ _Repositories/ProductRepository.cs
@@ -0,0 +1,91 @@
+﻿using Supermarket_mvp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp._Repositories
+{
+    internal class ProductRepository : BaseRepository, IProductRepository
+    {
+        public ProductRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void Add(ProductsModel productsModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(ProductsModel productsModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProductsModel> GetAll()
+        {
+            //throw new NotImplementedException();
+            var productsList = new List<ProductsModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Products ORDER BY Product_Id DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var productsModel = new ProductsModel();
+                        productsModel.Id = (int)reader["Product_Id"];
+                        productsModel.Name = reader["Product_Name"].ToString();
+                        productsModel.Observation = reader["Product_Observation"].ToString();
+                        productsList.Add(productsModel);
+                    }
+                }
+            }
+            return productsList;
+        }
+
+        public IEnumerable<ProductsModel> GetByValue(string value)
+        {
+            //throw new NotImplementedException();
+            var productsList = new List<ProductsModel>();
+            int productsId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+            string productName = value;
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"SELECT * FROM Products 
+                                        WHERE Product_Id=@id or Product_Name LIKE @name+ '%'
+                                        ORDER BY Product_Id DESC";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = productsId;
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = productName;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var productsModel = new ProductsModel();
+                        productsModel.Id = (int)reader["Product_Id"];
+                        productsModel.Name = reader["Product_Name"].ToString();
+                        productsModel.Observation = reader["Product_Observation"].ToString();
+                        productsList.Add(productsModel);
+                    }
+                }
+            }
+
+            return productsList;
+        }
+    }
+}
