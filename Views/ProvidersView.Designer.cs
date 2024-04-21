a96ae8f424b0461e4eb8638508d0230589f90f3e
Julian Vega <juliangomvega@gmail.com>
2024-04-20 21:56:21 -0500

    Providers View

---
+++ Views/ProvidersView.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Supermarket_mvp.Views
+{
+    partial class ProvidersView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private TabControl tabControl1;
+        private TabPage tabPageProvidersList;
+        private TabPage tabPageProvidersDetail;
+        private Label label2;
+        private TextBox TxtSearch;
+        private Button BtnSearch;
+        private DataGridView DgProviders;
+        private Button BtnClose;
+        private Button BtnDelete;
+        private Button BtnEdit;
+        private Button BtnNew;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private TextBox TxtProvidersObservation;
+        private TextBox TxtProvidersName;
+        private TextBox TxtProvidersId;
+        private Button BtnCancel;
+        private Button BtnSave;
+    }
+}
